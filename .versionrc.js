aa25f20a4d85d5babfa8747eeebfcbc03e705623
0xjcf <jflores717@gmail.com>
2024-12-15 22:18:04 -0500

    chore: automate pull request creation and auto-merge for releases

--- .versionrc.js
+++ .versionrc.js
@@ -1,11 +1,11 @@
-module.exports = {
+export default {
   types: [
     { type: "feat", section: "Features" },
     { type: "fix", section: "Bug Fixes" },
     { type: "docs", section: "Documentation" },
     { type: "refactor", section: "Refactoring" },
     { type: "build", section: "Build System" },
-    { type: "ci", section: "Continuous Integration" },
+    { type: "ci", section: "Continuous Integration", hidden: true },
     { type: "chore", section: "Chores", hidden: true },
     { type: "style", section: "Styling", hidden: true },
     { type: "test", section: "Tests", hidden: true },
