5f338578b4ce57207af989c20df0aaaae7138c7c
0xjcf <jflores717@gmail.com>
2024-12-31 10:21:44 -0500

    feat: expose RenderArgs for decorators

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export { default as createMobXAdapter } from "./adapters/MobxAdapter";
 export { default as createXStateAdapter } from "./adapters/XStateAdapter";
 export { default as createReduxAdapter } from "./adapters/ReduxAdapter";
 export { setGlobalStyles } from "./globalStyles";
+export type { RenderArgs } from "./RenderArgs";
