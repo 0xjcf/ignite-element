7de25633566929a960e42a955fbd6f009b5a4f20
JOSΞ <jflores717@gmail.com>
2026-05-27 14:47:56 -0400

    feat: define v3 public api boundary

--- packages/ignite-element/src/index.ts
+++ packages/ignite-element/src/index.ts
@@ -10,29 +10,6 @@ export type {
 } from "ignite-core";
 export { event, StateScope } from "ignite-core";
 export type {
-	IgniteConfig,
-	IgniteLoggingLevel,
-	IgniteRendererId,
-	IgniteRenderStrategyId,
-} from "ignite-renderer";
-export {
-	defineIgniteConfig,
-	getIgniteConfig,
-	setGlobalStyles,
-} from "ignite-renderer";
-export { loadIgniteConfig } from "./config/loadIgniteConfig";
-export {
-	type AdapterPack,
-	type BaseRenderArgs as IgniteRenderArgs,
-	type ComponentFactory,
-	default as igniteElementFactory,
-} from "./IgniteElementFactory";
-export type { RenderArgs } from "./RenderArgs";
-export {
-	registerRenderStrategy,
-	resolveConfiguredRenderStrategy,
-} from "./renderers/resolveConfiguredRenderStrategy";
-export type {
 	IgniteEventExpectation,
 	IgniteEventPayloadExpectation,
 	IgniteStateExpectation,
